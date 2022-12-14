606faecde6505a6a3738351745a85f1b295788b7
Rowaaantje <114189377+Rowaaantje@users.noreply.github.com>
2022-12-14 20:30:11 +0100

    Text Adventure
    Text Adventure Import

---
+++ Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Zuul;
+
+
+namespace Zuul
+{
+    public class Player
+    {
+        private int health;
+
+        public Room CurrentRoom { get; set; }
+        public int Health
+        {
+            get { return health; }
+        }
+        public Player()
+        {
+            CurrentRoom = null;
+
+            health = 100;
+        }
+
+        public void Damage(int amount) //Substract Healt
+        {
+            health -= amount;
+        }
+        public void Heal(int amount) //Add Health
+        {
+            health += amount;
+        }
+
+        public bool IsAlive() //Check if Player is alive
+        {
+            return (health > 0);
+        }
+    }
+}
+
